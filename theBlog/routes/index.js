94cd7faca01ad41900359318de88a780f6391b2f
David Hasenjaeger <davehasen@gmail.com>
2015-02-05 15:04:17 -0800

    submits user to users on refresh

--- theBlog/routes/index.js
+++ theBlog/routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var development = require('../knexfile.js').development;
+var knex = require('knex')(development);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.query);
+  var username = req.query.username;
+  var password = req.query.password;
+  var email = req.query.email;
+  knex('users').insert({name: username}).then(function(){
+
+  });
   res.render('index', { title: 'Twice Twitter' });
 });
 
