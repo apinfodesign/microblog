0cf0dffc4d5d1d7402329c8e529f1b471292b824
David Hasenjaeger <davehasen@gmail.com>
2015-02-05 23:14:29 -0800

    changes to registration
    there is now a registration page that is separate from the home page. The
    registration page also doesn't encounter errors when fields are not defined.

--- theBlog/routes/index.js
+++ theBlog/routes/index.js
@@ -7,9 +7,39 @@ var knex = require('knex')(development);
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  var usernameReg = req.query.usernameRegistered;
+  var passwordReg = req.query.passwordRegistered;
+  	console.log(usernameReg + " is usernameReg");
+ 	console.log(passwordReg + " is passwordReg");
+	res.render('index', {title: 'Better Twitter'});
+});
+
+function checkUniqueName (username, callback){
+	var result= true;
+	//gets all the names in users
+	knex('users').where({name: username}).select('name').then(function(allnames){ 
+		if (allnames.length!==0){
+			result=false;
+		}  //if any results returned, then false	
+  	callback(result);
+ 	});
+ 	
+};
+
+router.get('/registration/', function (req, res, next) {
+
   var usernameTemp = req.query.username;
+  if (usernameTemp === undefined) {
+  	usernameTemp = '';
+  }
   var passwordTemp = req.query.password;
+  if (passwordTemp === undefined) {
+  	passwordTemp = '';
+  }
   var emailTemp = req.query.email;
+  if (emailTemp === undefined) {
+  	emailTemp = '';
+  }
 
   var errorMessage = '';
   var error = false;
@@ -41,36 +71,17 @@ router.get('/', function(req, res, next) {
 		checkUniqueName(usernameTemp, function(result){ 
 			if (result) {
 				knex('users').insert({name: usernameTemp, password: passwordTemp, email: emailTemp}).then();
-				res.render('index', { title: 'Better Twitter', message: errorMessage });
+				res.render('registration', { title: 'Better Twitter', message: errorMessage });
 			}
 			else{
 				errorMessage ='<p>Username already exists.  Please sign in or choose a different user name.</p>';
-				res.render('index', { title: 'Better Twitter', message: errorMessage });
+				res.render('registration', { title: 'Better Twitter', message: errorMessage });
 			}	
 		});
 	} else {
-		res.render('index', { title: 'Better Twitter', message: errorMessage });
+		res.render('registration', { title: 'Better Twitter', message: errorMessage });
 	}
-
-
-  var usernameReg = req.query.usernameRegistered;
-  var passwordReg = req.query.passwordRegistered;
-  	console.log(usernameReg + " is usernameReg");
- 	console.log(passwordReg + " is passwordReg");
- 
 });
-
-var checkUniqueName = function(username, callback){
-	var result= true;
-	//gets all the names in users
-	knex('users').where({name: username}).select('name').then(function(allnames){ 
-		if (allnames.length!==0){
-			result=false;
-		}  //if any results returned, then false	
-  	callback(result);
- 	});
- 	
-};
 
 
 
