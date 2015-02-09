69915f4d8f4d4a3917ddd1cf7aaaaf680366dc68
David Hasenjaeger <davehasen@gmail.com>
2015-02-09 14:40:19 -0800

    Strange flags removed

--- theBlog/public/javascripts/logoutscript.js
+++ theBlog/public/javascripts/logoutscript.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-
-
-#('logout').on(click)function(){
- 	docCookies.removeItem('last-login-time');
-=======
 var logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener("click", function() {
 	document.cookie = 'last-login-time=0';
 	document.location.reload();
->>>>>>> 8f2cc93eadc07e7bbe3d4e0298418b43843caa36
 });
